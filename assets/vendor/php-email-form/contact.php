f014880c93c988018bd9ab067fab69d42a3cd06d
RAJ PATEL <74208647+rajpatel8@users.noreply.github.com>
2021-07-26 20:08:45 +0530

    Add files via upload

---
+++ assets/vendor/php-email-form/contact.php
@@ -0,0 +1,14 @@
+<?php
+
+    $name = $_POST['name'];
+    $subject = $_POST['email'] ;
+    $mailform = $_POST['subject'] ;
+    $message = $_POST['message'] ;
+
+    $mailTo = "rajphx299@gmail.com"
+    $headers = "From: ".$mailform ;
+    $txt = "You have recived an e-mail from".$name. ".\n\n".$message;
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+
+?>
